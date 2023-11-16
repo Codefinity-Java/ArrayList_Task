a404c3519966ccc104a46ddeda0aff1346d6d425
Codefinity-Java <dl@codefinity.com>
2023-11-16 14:24:37 +0200

    added solution

--- src/main/java/codefinity/Library.java
+++ src/main/java/codefinity/Library.java
@@ -1,27 +1,46 @@
 package codefinity;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
+    List<Book> books = new ArrayList<>();
 
     public void addBook(Book book) {
-
+        books.add(book);
     }
 
     public void removeBookById(int id) {
-
+        List<Book> booksToRemove = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getId() == id) {
+                booksToRemove.add(book);
+            }
+        }
+        books.removeAll(booksToRemove);
     }
 
     public List<Book> findBooksByAuthor(String author) {
-        return Collections.emptyList();
+        List<Book> booksWithSpecifiedAuthor = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getAuthor().equals(author)) {
+                booksWithSpecifiedAuthor.add(book);
+            }
+        }
+        return booksWithSpecifiedAuthor;
     }
 
     public List<Book> findBooksPublishedAfterYear(int year) {
-        return Collections.emptyList();
+        List<Book> booksPublishedAfterYear = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getYear() > year){
+                booksPublishedAfterYear.add(book);
+            }
+        }
+        return booksPublishedAfterYear;
     }
 
     public void displayAllBooks() {
-
+        System.out.println(books);
     }
 }
